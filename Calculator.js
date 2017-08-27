4526fcc8eebc216d948ddecf720e4bbc0f67fe12
Matt Rustenholtz <matthewdrl@gmail.com>
2017-08-27 14:12:03 -0700

    Create Practice Calculator

---
+++ Calculator.js
@@ -0,0 +1,93 @@
+/**
+ * Numbers
+ */
+var stackNum = [];
+
+/**
+ * Operators
+ */
+var stackOper = [];
+
+/**
+ * Show number on screen.
+ * @param {int} number - Number inputted to be displayed.
+ */
+function displayNumber(number) {
+    document.getElementById("display0").innerHTML = number;
+}
+
+function printDebug() {
+    console.log("StackNum:" + stackNum);
+    console.log("StackOper:" + stackOper);
+}
+
+/**
+ * Input a number.
+ * @param {string} number - the integer entered when a button is pressed.
+ */
+function enterNum(number) {
+    var num = parseInt(number);
+    stackNum.unshift(num);  
+    displayNumber(num);
+    printDebug();
+    
+    // other way to write: stackNum.unshift(parseInt(number));
+}
+
+/**
+ * Enters an math operation.
+ * @param {string} ops - the operator. Possible values: plus, minus, times, divide, and equals
+ */
+function enterOp(ops) {
+    if(ops === "equals") {
+        total();
+    } else {    
+        stackOper.unshift(ops);
+    }
+
+    printDebug();
+}
+
+/**
+ * This is will produce the total amount.
+ */
+function total() {
+    // step #1: pop the two values
+    var number1 = stackNum.shift();
+    var number2 = stackNum.shift();
+
+    // step #2: pop the operator
+    var operator = stackOper.shift();
+
+    // step #3: do the math based on operator
+    var result;
+    switch(operator) {
+        case "plus":
+            result = number2 + number1;
+            break;
+        case "minus":
+            result = number2 - number1;
+            break;
+        case "times":
+            result = number2 * number1;
+            break;
+        case "divide":
+            result = number2 / number1;
+            break;        
+    }
+
+    // step #4: push the result into stackNum
+    stackNum.unshift(result);
+
+    // step #5: display and console.log the result.
+    displayNumber(result);
+    console.log(result);
+}
+
+function reset() {
+    displayNumber(0);
+    stackNum = [];
+    stackOper = [];
+
+    printDebug();
+}
